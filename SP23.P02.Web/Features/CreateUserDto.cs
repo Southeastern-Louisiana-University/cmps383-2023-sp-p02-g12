301d1a6870cf754e413c35fe49d62f93084f46be
kchelle robinson <rkchelle64@gmail.com>
2023-02-06 12:35:57 -0600

    Added to rest of the dtos and made the various entities for auth

---
+++ SP23.P02.Web/Features/CreateUserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SP23.P02.Web.Features
+{
+    public class CreateUserDto
+    {
+        public string   UserName { get; set; }
+        public string[] Roles { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ SP23.P02.Web/Features/Role.cs
@@ -0,0 +1,6 @@
+﻿namespace SP23.P02.Web.Features
+{
+    public class Role
+    {
+    }
+}
