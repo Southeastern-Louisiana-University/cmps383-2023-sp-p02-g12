f51f9f32e643f7a9ac98da64c485e84f17365c79
souplamp <camden.mcall@gmail.com>
2023-02-15 10:16:24 -0600

    added spa extensions to fix bug

--- SP23.P02.Web/Features/CreateUserDto.cs
+++ SP23.P02.Web/Features/CreateUserDto.cs
@@ -2,7 +2,7 @@
 {
     public class CreateUserDto
     {
-        public string   UserName { get; set; }
+        public string UserName { get; set; }
         public string[] Roles { get; set; }
         public string Password { get; set; }
     }
